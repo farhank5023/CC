80a1b4ddc2a26a5b95f25be983e4ee6a90e2beb2
farhank5023 <farhank5023@gmail.com>
2023-09-07 13:47:07 +0530

    updated on 7 -09

---
+++ scripts/seed.ts
@@ -0,0 +1,27 @@
+const {PrismaClient}=require("@prisma/client");
+
+const db =new PrismaClient();
+async function main(){
+    try {
+        await db.category.createMany({
+
+
+            data:[
+
+                {name:"Famous People"},
+                {name:"Movies & TV"},
+                {name:"Famous People"},
+                {name:"Musicians"},
+                {name:"Games"},
+                {name:"Animals"},
+                {name:"Philosophy"},
+                {name:"Scientists"},
+            ]
+        })
+    }catch (error) {
+        console.log("Error seeding deafult categories ",error);
+    }finally {
+        await db.$disconnect();
+    }
+};
+main();
